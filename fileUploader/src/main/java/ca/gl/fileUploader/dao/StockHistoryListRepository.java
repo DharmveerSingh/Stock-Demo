45db99b5ed36ed9be7d560db8dcdf8d2cbd1dca7
Dharamveer singh <mailtodharmveer@gmail.com>
2019-03-14 16:02:09 +0530

    Added cron job to remove stocks weekly history before 1 week

--- fileUploader/src/main/java/ca/gl/fileUploader/dao/StockHistoryListRepository.java
+++ fileUploader/src/main/java/ca/gl/fileUploader/dao/StockHistoryListRepository.java
@@ -1,14 +1,19 @@
 package ca.gl.fileUploader.dao;
 
+import org.springframework.data.couchbase.core.query.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import ca.gl.fileUploader.model.StockHistoryList;
+import reactor.core.publisher.Flux;
 
 /**
  * The Interface StockHistoryListRepository.
  */
 @Repository
 public interface StockHistoryListRepository extends ReactiveCrudRepository<StockHistoryList, String>{
+
+	@Query("#{#n1ql.selectEntity} where  META().id LIKE \"REGULAR::HISTORY::%\"")
+	Flux<StockHistoryList> getHistoryForAll();
 
 }
